791a4a734b8c4005c80a96805ed4ffe9a2e0a1b4
KgothatsoNaka <u23688794@tuks.co.za>
2025-03-03 19:42:57 +0200

    first commit

---
+++ KgothatsoStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KgothatsoStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentView()
+        {
+            List<Models.StudentInfo> students = new List<Models.StudentInfo>();
+
+            students.Add(new Models.StudentInfo { StudentID = 23688794, StudentName = "Kgothatso", StudentSurname = "Naka", StudentEmail = "u23688794@tuks.co.za" });
+            students.Add(new Models.StudentInfo { StudentID = 23708795, StudentName = "Jose Edu", StudentSurname = "Solsona", StudentEmail = "u23708795@tuks.co.za" });
+            students.Add(new Models.StudentInfo { StudentID = 21543667, StudentName = "Thokozille", StudentSurname = "Sibanyoni", StudentEmail = "u21543667@tuks.co.za" });
+            students.Add(new Models.StudentInfo { StudentID = 22609301, StudentName = "Usiphile", StudentSurname = "Gumbi", StudentEmail = "u22609301@tuks.co.za" });
+
+
+
+            return View(students);
+        }
+    }
+}
---
+++ KgothatsoStudyGroup/Models/StudentInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace KgothatsoStudyGroup.Models
+{
+	public class StudentInfo
+	{
+        [Display(Name = "Student ID")]
+        public int StudentID { get; set; }
+
+
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+
+
+        [Display(Name = "Surname")]
+        public string StudentSurname { get; set; }
+
+
+        [Display(Name = "Email Address")]
+        public string StudentEmail { get; set; }
+    }
+
+}
